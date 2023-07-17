6ab8166d7e648e891b203469b3a446def4ca7aa3
sakamotoyuuki <135770995+sakamotoyuuki@users.noreply.github.com>
2023-07-17 15:49:06 +0900

    Add files via upload

---
+++ kadai_021/confirmation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題21</title>
+</head>
+<body>
+    <h2>入力内容をご確認下さい</h2>
+    <p>お名前：<?php echo $_POST['user_name'];?></p>
+    <p>性別：<?php echo $_POST['user_gender'];?></p>
+    <p>メールアドレス：<?php echo $_POST['user_mail'];?></p>
+</body>
+</html>
